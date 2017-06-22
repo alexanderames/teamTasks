8f97c0534d9e3de589b38640ec93890ef8fd0f88
alexander ames <alexanderthomasames@gmail.com>
2017-06-21 19:48:05 -0700

    set up sign up

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,9 +1,62 @@
 import React, { Component } from 'react';
+import { firebaseApp } from '../firebase';
 
 class SignUp extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			email: '',
+			password: '',
+			error: {
+				message: ''
+			}
+		}
+	}
+
+	signUp() {
+		console.log(this.state);
+		const { email, password } = this.state; 
+		firebaseApp.auth().createUserWithEmailAndPassword(email, password)
+			.catch(error => {
+				this.setState({error});
+			})
+	}
+
 	render() {
 		return(
-			<div>SignUp</div>
+			<div className="row">
+	    	<form className="col s12">
+					<h2>SignUp</h2>
+					<div className="row">
+						<div className="input-field col s6">
+							<input
+								placeholder="email"
+								className="validate"
+								type="email"
+								onChange={event => this.setState({email: event.target.value})}
+							/>
+						</div>
+					</div>
+					<div className="row">
+						<div className="input-field col s6">
+							<input
+								placeholder="password"
+								type="password"
+								className="validate"
+								onChange={event => this.setState({password: event.target.value})}
+							/>
+						</div>
+					</div>
+							<button
+								className="waves-effect waves-light btn"
+								type="button"
+								onClick={() => this.signUp()}
+							>
+								Sign Up
+							</button>
+				</form>
+				<div>{this.state.error.message}</div>
+			</div>
 		)
 	}
 }
