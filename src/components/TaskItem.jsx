5ca87e369b91d41d76b6c576bb4c1edc512edfc8
alexander ames <alexanderthomasames@gmail.com>
2017-06-29 14:56:43 -0700

    complete goal using firebase ref

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -1,6 +1,17 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { completeTaskRef } from '../firebase';
 
 class TaskItem extends Component {
+	completeTask() {
+		//add to complete task on the database
+		//remove task from the task reference
+		const { email } = this.props.user;
+		const { title } = this.props.task;
+		console.log('email', email, 'title', title);
+		completeTaskRef.push({email, title});
+	}
+
 	render() {
 		const { email, title } = this.props.task;
 		return (
@@ -10,7 +21,7 @@ class TaskItem extends Component {
 				<a
 					type="button"
 					className="btn-floating green"
-					onClick={() => this.completeGoal()}
+					onClick={() => this.completeTask()}
 				>
 					<i className="material-icons">done</i>
 				</a>
@@ -19,4 +30,11 @@ class TaskItem extends Component {
 	}
 }
 
-export default TaskItem;
+function mapStateToProps(state) {
+	const { user } = state;
+	return {
+		user
+	}
+}
+
+export default connect()(TaskItem);
